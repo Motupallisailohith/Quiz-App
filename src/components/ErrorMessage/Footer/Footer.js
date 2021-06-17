5c95a150fd141179a06b4ca454128c02fd411a06
Motupalli Sailohith <42695240+Motupallisailohith@users.noreply.github.com>
2021-06-17 22:04:48 +0530

    Add files via upload

---
+++ src/components/ErrorMessage/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ with Oysters
+        
+      </div>
+    );
+  };
+  
+  export default Footer;
